da0a7a2830714baaf449ab49dc049f18ed092627
MAXON-BY <max_on.89@mail.ru>
2017-07-17 18:14:10 +0300

    Готовая рабочая версия V1.0

--- js/common.js
+++ js/common.js
@@ -10,17 +10,26 @@ $(function() {
             data: th.serialize()
         }).done(function() {
             $('.hid').hide();
-            $('.thanks').show();
-            $('.thanks .popup').animate({
+            $('.popup-hid').hide();
+            $('#popup-ok').show();
+            $('.popup').animate({
                 top: '30%',
                 opacity: 1
             }, 800 );
             th.trigger("reset");
             $('.popup-sms').slideUp( 100 ).delay( 800 ).fadeIn( 400 );
         }).success (function(){
-            $(dataLayer.push({'event': 'west_coast_nadia'}));
+           // $(dataLayer.push({'event': 'west_coast_nadia'}));
+        }).error(function () {
+            $('.popup-hid').hide();
+            $('#popup-error').show();
+            $('.popup').animate({
+                top: '30%',
+                opacity: 1
+            }, 800 );
+            th.trigger("reset");
+            $('.popup-sms').slideUp( 100 ).delay( 800 ).fadeIn( 400 );
         });
-
         return false;
     });
 
@@ -91,3 +100,32 @@ $(".rev-img").click(function() {
     $(".rev-img").removeClass("active").eq($(this).index()).addClass("active");
     $(".rev-block").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
+
+// date
+$( function() {
+   $( ".date" ).datepicker({minDate: 0});
+} );
+
+//popup sms CLOSE
+$('.close').click(function(){
+    $('.popup-hid').hide();
+});
+
+/* open form quote */
+$('#free-quote').click(function(){
+    $('#popup-quote').show();
+    $('.popup').animate({
+        top: '15%',
+        opacity: 1
+    }, 800 );
+});
+
+/* open form book now */
+$('.book-now').click(function(e){
+    e.preventDefault();
+    $('#popup-book-now').show();
+    $('.popup').animate({
+        top: '15%',
+        opacity: 1
+    }, 800 );
+});
--- mail.php
+++ mail.php
@@ -1,52 +1,136 @@
 <?php
+if (!empty($_POST)) {
 
-$method = $_SERVER['REQUEST_METHOD'];
+//theleadtornado@gmail.com
+    $to = 'maxon.pro2015@gmail.com';
+    $subject = 'LEAD FROM West Coast';
+    $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
 
-//Script Foreach
-$c = true;
-if ( $method === 'POST' ) {
 
-	$project_name = trim($_POST["project_name"]);
-	$admin_email  = 'max_on.89@mail.ru';
-	$form_subject = trim($_POST["form_subject"]);
+// данные с мыла
+    $formid = trim($_POST["formid"]);
+    $location = trim($_POST["location"]);
+    $name = trim($_POST["name"]);
+    $phone = trim($_POST["phone"]);
+    $text = trim($_POST["text"]);
+    $email = trim($_POST["email"]);
+    $size_of_move = trim($_POST["size"]);
+    $code_from = trim($_POST["zip"]);
+    $code_to = trim($_POST["zipto"]);
+    $date = trim($_POST["date"]);
 
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-		</tr>
-		";
-	}
+    $start = array('utm_source', 'utm_campaign', 'utm_keyword', 'utm_geo', 'utm_matchtype', 'utm_site', 'utm_placement', 'utm_position', 'utm_ad');
+    $finish = array('Source', 'ID Campaign', 'Keyword', 'GEO', 'Matchtype', 'Google', 'Placement', 'AD Position', 'ID AD');
+    $repl = array('+','%20','%2B','%3A','%2F','%3D','%26','%23','%3F');
+    $replto = array(' ',' ','+',':','/','=','&','#','?');
+    preg_match_all('/([a-zA-Z_]+?)=([a-zA-Z0-9+:\/.%]+)/', $_SERVER['HTTP_REFERER'], $out);
+    for ($i=0;$i<count($out[0]);$i++)
+    {
+        $advertising .= "<tr><td class=\"bold\">".str_replace($start, $finish, $out[1][$i]).':</td><td>'.str_replace($repl, $replto, $out[2][$i])."</td></tr>";
+    }
+
+
+// текст письма
+    $message .= '
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">  
+  <title>LEAD FROM West Coast</title>
+  <style>
+   
+    body {
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
+    }
+    table {
+	width: 100%;
+    }
+    h1 {
+    font-size:medium;
+    }
+    caption {
+     text-align:left;
+     margin:1% 0;
+     padding:5px;
+     background-color: #E6E6E6;
+     font-weight:600;
+	 }
+	 td {
+	 width:50%
+	 }	 
+	 .subinfo {
+	 background-color: #E6E6E6;
+	 font-size:x-small;
+	 line-height:1;
+	 padding:5px;
+	 }
+	 .bold {
+	 font-weight:bold;
+	 }
+  </style>
+</head>
+<body>
+<h1>LEAD FROM West Coast</h1>
+<table>
+<caption>Lead Information:</caption>
+	<tr>
+		<td class="bold">Cell Phone:</td>
+		<td>'.$phone.'</td>
+	</tr>
+	<tr>
+		<td class="bold">Email:</td>
+        <td>'.$email.'</td>
+	</tr>	
+	<tr>
+		<td class="bold">Zip code from:</td>
+		<td>'.$code_from.'</td>
+	</tr>
+    <tr>
+		<td class="bold">Zip code to:</td>
+		<td>'.$code_to.'</td>
+	</tr>
+    <tr>
+		<td class="bold">Moving Date:</td>
+		<td>'.$date.'</td>
+	</tr>
+    <tr>
+		<td class="bold">Size of Move:</td>
+		<td>'.$size_of_move.'</td>
+	</tr>
+    <tr>
+		<td class="bold">Information:</td>
+		<td>'.$text.'</td>
+	</tr>
+</table>';
+
+    if($advertising!="")
+        $message .= '
+<table>
+	<caption>Advertising Information:</caption>	
+	'.$advertising.'
+</table>';
+
+    $message .= '
+<div class="subinfo">
+  <p>Form id: '.$formid.'</p>
+  <p>Visitor IP adress: '.$_SERVER['HTTP_X_FORWARDED_FOR'].'</p>  
+  <p>Form submitted from: '. preg_replace('/\?.*/', '', $_SERVER['HTTP_REFERER']).'</p>
+</div>
+</body>
+</html>
+';
+
+    $headers = 'Content-type: text/html; charset="utf-8"';
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
+    $headers .= "From: West Coast <admin@west-coastmoving.com>\r\n";
+    if($phone!=''||$email!='') {
+        mail($to, $subject, $message, $headers);
+    }
+    else {return true;
+    }
+
 }
-} else if ( $method === 'GET' ) {
-
-	$project_name = trim($_GET["project_name"]);
-    $admin_email  = 'max_on.89@mail.ru';
-	$form_subject = trim($_GET["form_subject"]);
-
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-		</tr>
-		";
-	}
+else {return true;
 }
-}
-
-$message = "<table style='width: 100%;'>$message</table>";
-
-function adopt($text) {
-	return '=?UTF-8?B?'.base64_encode($text).'?=';
-}
-
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
-
-mail($admin_email, adopt($form_subject), $message, $headers );
+?>
